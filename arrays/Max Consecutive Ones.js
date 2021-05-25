258422da284683694c565fe67885e3e274079246
Ujjwal Garad <ugarad@mathworks.com>
2021-05-25 01:07:46 -0400

    init

--- arrays/Max Consecutive Ones.js
+++ arrays/Max Consecutive Ones.js
@@ -1,3 +1,11 @@
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+
 function countConsOnes(input){
     let prevCount = 0, curCount = 0;
 
